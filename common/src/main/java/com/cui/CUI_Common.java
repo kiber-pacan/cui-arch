3752cd9ce0cb99baa79448ebc76c8e2b122c7a4e
kiber-pacan <markpustog@gmail.com>
2024-09-25 14:46:43 +0100

    Removed redundant colors

--- common/src/main/java/com/cui/CUI_Common.java
+++ common/src/main/java/com/cui/CUI_Common.java
@@ -4,10 +4,7 @@ import net.minecraft.util.math.Vec3d;
 
 public final class CUI_Common {
     public static final String MODID = "cui";
-    public static final float RED = 0.09f;
-    public static final float GREEN = 0.81f;
-    public static final float BLUE = 0.38f;
-    public static final float CBONUS = 0.32f;
+    public static Colors colors = new Colors();
 
 
     public static void initializeServer() {}
