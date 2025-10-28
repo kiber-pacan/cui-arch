166f965b257cf53dfef0f9c383851e3e6ec50c64
kiber-pacan <markpustog@gmail.com>
2025-10-29 00:59:20 +0500

    brighter titles

--- common/src/main/java/com/cui/mixin/client/TitleMixin.java
+++ common/src/main/java/com/cui/mixin/client/TitleMixin.java
@@ -3,7 +3,6 @@ package com.cui.mixin.client;
 import com.cui.CUI_Config;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.TitleScreen;
-import net.minecraft.client.gui.screens.recipebook.RecipeBookComponent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -15,8 +14,9 @@ import java.awt.*;
 public class TitleMixin {
     @Inject(at = @At(value = "HEAD"), method = "render")
     private void renderHead(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick, CallbackInfo ci) {
-        Color colors = CUI_Config.HANDLER.instance().color;
-        guiGraphics.setColor((float) colors.getRed() / 255, (float) colors.getGreen() / 255, (float) colors.getBlue() / 255, 1);
+        Color color = CUI_Config.HANDLER.instance().color;
+        color.brighter().brighter().brighter();
+        guiGraphics.setColor((float) color.getRed() / 255, (float) color.getGreen() / 255, (float) color.getBlue() / 255, 1);
     }
 
     @Inject(at = @At(value = "TAIL"), method = "render")
