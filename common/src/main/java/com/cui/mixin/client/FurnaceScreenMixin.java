cdc8b2aaf57ccbb3e691caf7d08e27f4bee3731f
kiber-pacan <markpustog@gmail.com>
2025-11-08 18:59:14 +0500

    book

--- common/src/main/java/com/cui/mixin/client/FurnaceScreenMixin.java
+++ common/src/main/java/com/cui/mixin/client/FurnaceScreenMixin.java
@@ -1,8 +1,13 @@
 package com.cui.mixin.client;
 
 
+import com.cui.CUI;
+import com.mojang.authlib.minecraft.client.MinecraftClient;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.inventory.AbstractFurnaceScreen;
+import net.minecraft.client.gui.screens.recipebook.AbstractFurnaceRecipeBookComponent;
+import net.minecraft.world.item.Items;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -12,25 +17,22 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(AbstractFurnaceScreen.class)
 public class FurnaceScreenMixin {
-	/*
 	// Background
-	@Final
-	@Shadow
-	public AbstractFurnaceRecipeBookScreen recipeBook;
 
-	@Inject(at = @At(value = "HEAD"), method = "renderBg")
-	private void renderTail(GuiGraphics guiGraphics, float partialTick, int mouseX, int mouseY, CallbackInfo ci) {
-		int a = MinecraftClient.getInstance().getWindow().getScaledWidth(), b = MinecraftClient.getInstance().getWindow().getScaledHeight();
-		if (recipeBook.isOpen()) {
-			context.drawItem(Items.KNOWLEDGE_BOOK.getDefaultStack(), (int) (((float) a / 2) + 11), (int) (((float) b / 2) - 48));
-		} else {
-			context.drawItem(Items.KNOWLEDGE_BOOK.getDefaultStack(), (int) (((float) a / 2) - 66), (int) (((float) b / 2) - 48));
-		}
-		context.setShaderColor(colors.r, colors.g, colors.b, 1);
-	}
+	@Shadow
+    @Final
+    public AbstractFurnaceRecipeBookComponent recipeBookComponent;
 
 	@Inject(at = @At(value = "TAIL"), method = "renderBg")
-	private void renderHead(GuiGraphics guiGraphics, float partialTick, int mouseX, int mouseY, CallbackInfo ci) {
-		context.setShaderColor(1, 1, 1, 1);
-	}*/
+	private void renderTail(GuiGraphics guiGraphics, float partialTick, int mouseX, int mouseY, CallbackInfo ci) {
+        guiGraphics.setColor(1, 1, 1, 1);
+
+		int a = Minecraft.getInstance().getWindow().getGuiScaledWidth(), b = Minecraft.getInstance().getWindow().getGuiScaledHeight();
+		if (recipeBookComponent.isVisible()) {
+            guiGraphics.renderItem(Items.KNOWLEDGE_BOOK.getDefaultInstance(), (int) (((float) a / 2) + 11), (int) (((float) b / 2) - 48));
+		} else {
+            guiGraphics.renderItem(Items.KNOWLEDGE_BOOK.getDefaultInstance(), (int) (((float) a / 2) - 66), (int) (((float) b / 2) - 48));
+		}
+        guiGraphics.setColor(CUI.cuiConfig.r, CUI.cuiConfig.g, CUI.cuiConfig.b, 1);
+	}
 }
