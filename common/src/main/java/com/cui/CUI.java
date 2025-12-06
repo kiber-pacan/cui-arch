2706b50974c72f837256442c2d468cb0a3549829
kiber-pacan <markpustog@gmail.com>
2025-12-06 23:23:05 +0500

    color mix method

--- common/src/main/java/com/cui/CUI.java
+++ common/src/main/java/com/cui/CUI.java
@@ -3,9 +3,11 @@ package com.cui;
 
 import com.cui.config.CUI_Config;
 import com.mojang.blaze3d.platform.InputConstants;
+import com.mojang.blaze3d.shaders.ShaderType;
 import dev.architectury.event.events.client.ClientLifecycleEvent;
 import dev.architectury.registry.client.keymappings.KeyMappingRegistry;
 import net.minecraft.client.KeyMapping;
+import net.minecraft.resources.ResourceLocation;
 
 import java.io.IOException;
 import java.util.logging.Logger;
@@ -17,6 +19,25 @@ public final class CUI {
     public static KeyMapping OPEN_CUI_CONFIG;
 
     public static CUI_Config cuiConfig = new CUI_Config();
+
+    public static int mixColors(int color1, int color2) {
+        int a1 = (color1 >> 24) & 0xFF;
+        int r1 = (color1 >> 16) & 0xFF;
+        int g1 = (color1 >> 8) & 0xFF;
+        int b1 = color1 & 0xFF;
+
+        int a2 = (color2 >> 24) & 0xFF;
+        int r2 = (color2 >> 16) & 0xFF;
+        int g2 = (color2 >> 8) & 0xFF;
+        int b2 = color2 & 0xFF;
+
+        int a = (a1 + a2) / 2;
+        int r = (r1 + r2) / 2;
+        int g = (g1 + g2) / 2;
+        int b = (b1 + b2) / 2;
+
+        return (a << 24) | (r << 16) | (g << 8) | b;
+    }
 
     public static void initializeServer() {}
 
@@ -33,7 +54,7 @@ public final class CUI {
         #if MC_VER >= V1_20_4
         ClientLifecycleEvent.CLIENT_STARTED.register((minecraft) -> {
             try {
-                cuiConfig.loadConfig();
+                 cuiConfig.loadConfig();
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
