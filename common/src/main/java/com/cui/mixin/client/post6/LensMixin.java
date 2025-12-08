f5b0ad3cd2fce91447acd3f1cedfde6e25f71272
kiber-pacan <markpustog@gmail.com>
2025-12-08 17:31:20 +0500

    new mixins

---
+++ common/src/main/java/com/cui/mixin/client/post6/LensMixin.java
@@ -0,0 +1,31 @@
+package com.cui.mixin.client.post6;
+
+import com.cui.CUI;
+import com.mojang.blaze3d.pipeline.RenderPipeline;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.components.toasts.TutorialToast;
+import net.minecraft.client.gui.screens.recipebook.RecipeBookComponent;
+import net.minecraft.client.renderer.RenderPipelines;
+import net.minecraft.resources.ResourceLocation;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(#if MC_VER >= V1_21_6 RecipeBookComponent.class #else Minecraft.class #endif)
+public class LensMixin {
+    #if MC_VER >= V1_21_6
+    @Shadow private int getYOrigin() { return 0; }
+
+    @Shadow private int getXOrigin() { return 0; }
+
+    @Inject(at = @At(value = "TAIL"), method = "render")
+    private void injected(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick, CallbackInfo ci) {
+        int i = this.getXOrigin();
+        int j = this.getYOrigin();
+        guiGraphics.blit(RenderPipelines.GUI_TEXTURED, ResourceLocation.withDefaultNamespace("textures/gui/recipe_book_lens.png"), i, j, 1.0F, 1.0F, 147, 166, 256, 256);
+    }
+    #endif
+}
