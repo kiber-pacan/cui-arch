d63ee536fb5de5a10e336e151f9906b5d146f6cb
kiber-pacan <markpustog@gmail.com>
2025-12-08 22:08:49 +0500

    bye bye, retarded method. it's time for semi-retarded method!

--- common/src/main/java/com/cui/mixin/client/post1_21_6/misc/GuiGraphicsMixin.java
+++ common/src/main/java/com/cui/mixin/client/post1_21_6/misc/GuiGraphicsMixin.java
@@ -7,34 +7,28 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
-// Probably retarded way of coloring white text, but who cares
+import java.awt.*;
+import java.util.logging.Logger;
+
+// Probably retarded way of coloring white/gray text, but who cares
 
 @Mixin(#if MC_VER >= V1_21_6 GuiGraphics.class #else Minecraft.class #endif)
 public class GuiGraphicsMixin {
     #if MC_VER >= V1_21_6
     @ModifyArg(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/render/state/GuiRenderState;submitText(Lnet/minecraft/client/gui/render/state/GuiTextRenderState;)V"), method = "drawString(Lnet/minecraft/client/gui/Font;Lnet/minecraft/util/FormattedCharSequence;IIIZ)V")
     private GuiTextRenderState injected(GuiTextRenderState renderState) {
-        if (((renderState.color >> 8) & 0xFF) == 170 || ((renderState.color >> 8) & 0xFF) == 255) {
-            int cuiColor = CUI.cuiConfig.getRGB();
-            int r1 = (cuiColor >> 16) & 0xFF;
-            int g1 = (cuiColor >> 8) & 0xFF;
-            int b1 = cuiColor & 0xFF;
+        // Checking if r = g = b
+        if (((renderState.color) & 0xFF) == ((renderState.color >> 8) & 0xFF) && ((renderState.color >> 8) & 0xFF) == ((renderState.color >> 16) & 0xFF)) {
+            float[] hsv = Color.RGBtoHSB((int) (CUI.cuiConfig.r * 255.0f), (int) (CUI.cuiConfig.g * 255.0f), (int) (CUI.cuiConfig.b * 255.0f), null);
+            float rawValue = ((renderState.color) & 0xFF) / 255.0f;
+            float value = (rawValue <= 0.5f) ? 1 - rawValue : rawValue;
 
-            int r2 = ((renderState.color) >> 16) & 0xFF;
-            int g2 = ((renderState.color) >> 8) & 0xFF;
-            int b2 = (renderState.color) & 0xFF;
-
-            int a = ((renderState.color) >> 24) & 0xFF;
-            int r = (r1 + r2) / 2;
-            int g = (g1 + g2) / 2;
-            int b = (b1 + b2) / 2;
-
-            int color = (a << 24) | (r << 16) | (g << 8) | b;
+            int color = (Color.getHSBColor(hsv[0], hsv[1] / 2.5f, value).getRGB());
 
             return new GuiTextRenderState(renderState.font, renderState.text, renderState.pose, renderState.x, renderState.y, color, renderState.backgroundColor, renderState.dropShadow, renderState.scissor);
-        } else {
-            return renderState;
         }
+
+        return renderState;
     }
     #endif
 }
