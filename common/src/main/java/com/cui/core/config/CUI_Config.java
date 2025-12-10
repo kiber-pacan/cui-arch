41d5c45dbad05541d447360d277781642ddf3698
kiber-pacan <markpustog@gmail.com>
2025-12-10 22:34:42 +0500

    broken config handling

--- common/src/main/java/com/cui/core/config/CUI_Config.java
+++ common/src/main/java/com/cui/core/config/CUI_Config.java
@@ -1,7 +1,6 @@
-package com.cui.config;
+package com.cui.core.config;
 
-import com.cui.CUI;
-import com.mojang.blaze3d.platform.NativeImage;
+import com.cui.core.CUI;
 import dev.architectury.platform.Platform;
 
 import java.awt.*;
@@ -13,9 +12,6 @@ import java.util.Optional;
 import java.util.Random;
 
 import com.electronwill.nightconfig.core.file.CommentedFileConfig;
-import com.electronwill.nightconfig.core.UnmodifiableConfig;
-import net.minecraft.client.renderer.texture.DynamicTexture;
-import net.minecraft.resources.ResourceLocation;
 
 
 public class CUI_Config {
@@ -28,8 +24,6 @@ public class CUI_Config {
 
     public Color color;
 
-    private CommentedFileConfig fileConfig;
-
     public CUI_Config(){}
 
     public void loadConfig() throws IOException {
@@ -39,29 +33,77 @@ public class CUI_Config {
 
         if (Files.exists(c)) {
 
-            this.fileConfig = CommentedFileConfig.builder(c)
+            CommentedFileConfig fileConfig = CommentedFileConfig.builder(c)
                     .concurrent()
                     .build();
 
-            this.fileConfig.load();
+            fileConfig.load();
 
             System.out.println("CUI CONFIG EXISTS");
 
-            Optional<String> rgb = this.fileConfig.getOptional("color");
-            Optional<String> alpha = this.fileConfig.getOptional("alpha");
-            Optional<String> enableButton = this.fileConfig.getOptional("button");
-            Optional<String> desaturation = this.fileConfig.getOptional("desaturation");
+            Optional<String> rgb = fileConfig.getOptional("color");
+            Optional<String> alpha = fileConfig.getOptional("alpha");
+            Optional<String> enableButton = fileConfig.getOptional("button");
+            Optional<String> desaturation = fileConfig.getOptional("desaturation");
 
-            this.r = rgb.map((hex) -> Integer.parseInt(hex.substring(0, 2), 16) / 255.0f).orElse(1.0f);
-            this.g = rgb.map((hex) -> Integer.parseInt(hex.substring(2, 4), 16) / 255.0f).orElse(1.0f);
-            this.b = rgb.map((hex) -> Integer.parseInt(hex.substring(4, 6), 16) / 255.0f).orElse(1.0f);
-            this.a = alpha.map(Float::parseFloat).orElse(1.0f);
+            color = randomPastel();
 
-            this.enableButton = enableButton.map(Boolean::parseBoolean).orElse(true);
-            this.desaturation = desaturation.map(Float::parseFloat).orElse(1.5f);
+            try {
+                this.r = rgb.map((hex) -> Integer.parseInt(hex.substring(0, 2), 16) / 255.0f).orElse(1.0f);
+            }
+            catch(Exception exception) {
+                this.r = (float) color.getRed() / 255;
+                saveConfig();
+                CUI.LOGGER.info(exception.getMessage());
+            }
+
+            try {
+                this.g = rgb.map((hex) -> Integer.parseInt(hex.substring(2, 4), 16) / 255.0f).orElse(1.0f);
+            }
+            catch(Exception exception) {
+                this.g = (float) color.getGreen() / 255;
+                saveConfig();
+                CUI.LOGGER.info(exception.getMessage());
+            }
+
+            try {
+                this.b = rgb.map((hex) -> Integer.parseInt(hex.substring(4, 6), 16) / 255.0f).orElse(1.0f);
+            }
+            catch(Exception exception) {
+                this.b = (float) color.getBlue() / 255;
+                saveConfig();
+                CUI.LOGGER.info(exception.getMessage());
+            }
+
+            try {
+                this.a = alpha.map(Float::parseFloat).orElse(1.0f);
+            }
+            catch(Exception exception) {
+                this.a = (float) color.getAlpha() / 255;
+                saveConfig();
+                CUI.LOGGER.info(exception.getMessage());
+            }
+
+            try {
+                this.enableButton = enableButton.map(Boolean::parseBoolean).orElse(true);
+            }
+            catch(Exception exception) {
+                this.enableButton = true;
+                saveConfig();
+                CUI.LOGGER.info(exception.getMessage());
+            }
+
+            try {
+
+                this.desaturation = desaturation.map(Float::parseFloat).orElse(1.5f);
+            }
+            catch(Exception exception) {
+                this.desaturation = 1.5f;
+                saveConfig();
+                CUI.LOGGER.info(exception.getMessage());
+            }
 
             this.color = new Color(r, g, b, a);
-
         } else {
             System.out.println("CUI CONFIG NOT EXISTS");
 
@@ -71,8 +113,8 @@ public class CUI_Config {
             this.g = (float) color.getGreen() / 255;
             this.b = (float) color.getBlue() / 255;
             this.a = (float) color.getAlpha() / 255;
+            this.enableButton = true;
             this.desaturation = 1.5f;
-            enableButton = true;
         }
     }
 
@@ -114,7 +156,7 @@ public class CUI_Config {
 
     public int getTextColor(int originalColor) {
         float[] hsv = Color.RGBtoHSB((int) (CUI.cuiConfig.r * 255.0f), (int) (CUI.cuiConfig.g * 255.0f), (int) (CUI.cuiConfig.b * 255.0f), null);
-        float avg = (((originalColor >> 0) & 0xFF) + ((originalColor >> 8) & 0xFF) + ((originalColor >> 16) & 0xFF)) / 3.0f;
+        float avg = (((originalColor) & 0xFF) + ((originalColor >> 8) & 0xFF) + ((originalColor >> 16) & 0xFF)) / 3.0f;
 
         return ((Color.getHSBColor(hsv[0], hsv[1] / desaturation, avg / 255.0f).getRGB()) & 0x00FFFFFF) | (((originalColor >> 24) & 0xFF) << 24);
     }
