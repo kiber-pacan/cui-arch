c5c7c398b9d3b28d2af25ca9e351ee31e9a57e1d
kiber-pacan <markpustog@gmail.com>
2025-12-11 17:59:15 +0500

    world creations tabs gradient

---
+++ common/src/main/java/com/cui/mixin/client/shitass/screen/world/CreateWorldTabs.java
@@ -0,0 +1,37 @@
+package com.cui.mixin.client.shitass.screen.world;
+
+
+import com.cui.core.CUI;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.components.tabs.TabNavigationBar;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.gui.screens.worldselection.CreateWorldScreen;
+import net.minecraft.network.chat.Component;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.awt.*;
+
+
+@Mixin(#if MC_VER < V1_21 TabNavigationBar.class #else Minecraft.class #endif)
+public abstract class CreateWorldTabs {
+    #if MC_VER < V1_21
+
+    @Shadow @Final private static int HEIGHT;
+    @Shadow private int width;
+
+    @Inject(at = @At(value = "TAIL"), method = "render")
+    private void renderHead(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick, CallbackInfo ci) {
+        float[] hsv = Color.RGBtoHSB((int) (CUI.cuiConfig.r * 255.0f), (int) (CUI.cuiConfig.g * 255.0f), (int) (CUI.cuiConfig.b * 255.0f), null);
+        guiGraphics.fillGradient(0, 0, width, HEIGHT,
+                ((int)(CUI.cuiConfig.a * 255) << 24) | (Color.getHSBColor(hsv[0], hsv[1], hsv[2] / 2.7f).getRGB() & 0x00FFFFFF),
+                ((int)(CUI.cuiConfig.a * 255) << 24) | (Color.getHSBColor(hsv[0], hsv[1], hsv[2] / 5.0f).getRGB() & 0x00FFFFFF)
+        );
+    }
+    #endif
+}
