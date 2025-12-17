3f60b1aee70295f4886ad506a33c37a39fb88d9f
kiber-pacan <markpustog@gmail.com>
2025-12-17 21:03:34 +0500

    shulker screen

---
+++ common/src/main/java/com/cui/mixin/client/screen/container/ShulkerMixin.java
@@ -0,0 +1,35 @@
+package com.cui.mixin.client.screen.container;
+
+#if MC_VER >= V1_21_6 import com.cui.core.CUI;
+import com.mojang.blaze3d.pipeline.RenderPipeline; #endif
+import com.cui.abs.core.rendering.gui.GuiRenderer;
+import com.cui.core.CUI;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.screens.inventory.LoomScreen;
+import net.minecraft.client.gui.screens.inventory.ShulkerBoxScreen;
+import net.minecraft.resources.ResourceLocation;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ShulkerBoxScreen.class)
+public class ShulkerMixin {
+    #if MC_VER >= V1_21_6
+    @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;blit(Lcom/mojang/blaze3d/pipeline/RenderPipeline;Lnet/minecraft/resources/ResourceLocation;IIFFIIII)V"), method = "renderBg")
+    private static void injected(GuiGraphics instance, RenderPipeline pipeline, ResourceLocation atlas, int x, int y, float u, float v, int width, int height, int textureWidth, int textureHeight) {
+        instance.blit(pipeline, atlas, x, y, u, v, width, height, textureWidth, textureHeight, CUI.cuiConfig.getRGB());
+    }
+    #elif MC_VER <= V1_21_5
+    @Inject(at = @At(value = "HEAD"), method = "renderBg")
+    private static void injected1(GuiGraphics guiGraphics, float partialTick, int mouseX, int mouseY, CallbackInfo ci) {
+        GuiRenderer.setShaderColor(guiGraphics, CUI.cuiConfig.getRGB());
+    }
+
+    @Inject(at = @At(value = "TAIL"), method = "renderBg")
+    private static void injected2(GuiGraphics guiGraphics, float partialTick, int mouseX, int mouseY, CallbackInfo ci) {
+        GuiRenderer.clearShaderColor(guiGraphics);
+    }
+    #endif
+}
