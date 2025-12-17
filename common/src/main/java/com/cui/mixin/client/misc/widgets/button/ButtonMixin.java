4cd716603d2f70c08a0335109619d9fecb35138a
kiber-pacan <markpustog@gmail.com>
2025-12-17 20:59:49 +0500

    moved button widget and also cleaned it a bit

--- common/src/main/java/com/cui/mixin/client/misc/widgets/button/ButtonMixin.java
+++ common/src/main/java/com/cui/mixin/client/misc/widgets/button/ButtonMixin.java
@@ -1,4 +1,4 @@
-package com.cui.mixin.client.misc.widgets;
+package com.cui.mixin.client.misc.widgets.button;
 
 import com.cui.abs.core.rendering.gui.GuiRenderer;
 import com.cui.core.CUI;
@@ -23,32 +23,21 @@ import com.cui.core.CUI;
 
 import java.awt.*;
 
-
+// Buttons
 @Mixin(AbstractButton.class)
 public class ButtonMixin {
     #if MC_VER >= V1_21_6
-    @ModifyArg(method = "renderWidget", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/ARGB;color(FI)I"), index = 1)
-    private int injected(int color) {
-        if (((color) & 0xFF) == ((color >> 8) & 0xFF) && ((color >> 8) & 0xFF) == ((color >> 16) & 0xFF)) {
-            return CUI.cuiConfig.getTextColor(color);
-        }
-
-        return color;
-    }
-
     @ModifyArg(method = "renderWidget", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;blitSprite(Lcom/mojang/blaze3d/pipeline/RenderPipeline;Lnet/minecraft/resources/ResourceLocation;IIIII)V"), index = 6)
-    private int injected1(int c) {
-        Color c1 = CUI.cuiConfig.color;
-        return ((c >> 24) & 0xFF) << 24 | (c1.getRed() & 0xFF) << 16 | (c1.getGreen() & 0xFF) << 8 | (c1.getBlue() & 0xFF);
+    private int injected1(int color) {
+        return CUI.cuiConfig.getRGBA(((color >>> 24) & 0xFF) / 255.0f);
     }
-
     #else
     @Redirect(at = @At(value = "INVOKE", target = #if MC_VER >= V1_21_3 "Lnet/minecraft/util/ARGB;white(F)I" #else "Lnet/minecraft/client/gui/GuiGraphics;setColor(FFFF)V" #endif, ordinal = 0), method = "renderWidget")
-    private #if MC_VER >= V1_21_3 int #else void #endif renderHead(#if MC_VER <= V1_21_1 GuiGraphics instance, float red, float green, float blue, #endif float alpha) {
+    private #if MC_VER >= V1_21_3 int #else void #endif renderHead(#if MC_VER <= V1_21_3 GuiGraphics instance, float red, float green, float blue, #endif float alpha) {
         #if MC_VER >= V1_21_3
         return CUI.cuiConfig.getRGB();
         #else
-        GuiRenderer.setShaderColor(instance, CUI.cuiConfig.getRGBA(alpha));
+        GuiRenderer.setShaderColor(instance, CUI.cuiConfig.getRGB());
         #endif
     }
     #endif
