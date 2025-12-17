48af1b12f06f2de3778cc8a415d9058f73e1c58b
kiber-pacan <markpustog@gmail.com>
2025-12-17 21:02:17 +0500

    refactoring

--- common/src/main/java/com/cui/mixin/client/misc/widgets/LogoMixin.java
+++ common/src/main/java/com/cui/mixin/client/misc/widgets/LogoMixin.java
@@ -1,5 +1,8 @@
 package com.cui.mixin.client.misc.widgets;
 
+import com.cui.abs.core.data.Rectangle;
+import com.cui.abs.core.data.ResourceBridge;
+import com.cui.abs.core.rendering.gui.GuiRenderer;
 import com.cui.core.CUI;
 #if MC_VER >= V1_21_6 import com.mojang.blaze3d.pipeline.RenderPipeline; #endif
 import net.minecraft.client.gui.GuiGraphics;
@@ -57,46 +60,31 @@ import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.function.Function;
+import java.util.logging.Logger;
 
 // Color minecraft logo in title screen
 
 @Mixin(LogoRenderer.class)
 public class LogoMixin {
-    #if MC_VER >= V1_21_6
-    @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;blit(Lcom/mojang/blaze3d/pipeline/RenderPipeline;Lnet/minecraft/resources/ResourceLocation;IIFFIIIII)V"), method = "renderLogo(Lnet/minecraft/client/gui/GuiGraphics;IFI)V")
-    private static void injected(GuiGraphics instance, RenderPipeline pipeline, ResourceLocation atlas, int x, int y, float u, float v, int width, int height, int textureWidth, int textureHeight, int color) {
-        int rgbWithoutAlpha = CUI.cuiConfig.getRGB() & 0x00FFFFFF;
-        int newColor = (color & 0xFF000000) | rgbWithoutAlpha;
-        instance.blit(pipeline, atlas, x, y, u, v, width, height, textureWidth, textureHeight, newColor);
-    }
-    #else
     #if MC_VER >= V1_21_3
-    @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;blit(Ljava/util/function/Function;Lnet/minecraft/resources/ResourceLocation;IIFFIIIII)V"), method = "renderLogo(Lnet/minecraft/client/gui/GuiGraphics;IFI)V")
-    private static void injected1(GuiGraphics instance, Function<ResourceLocation, RenderType> renderTypeGetter, ResourceLocation atlasLocation, int x, int y, float uOffset, float vOffset, int uWidth, int vHeight, int textureWidth, int textureHeight, int color) {
-        instance.blit(renderTypeGetter, atlasLocation, x, y, uOffset, vOffset, uWidth, vHeight, textureWidth, textureHeight, CUI.cuiConfig.getRGB());
+    @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;blit(" #if MC_VER >= V1_21_6 + "Lcom/mojang/blaze3d/pipeline/RenderPipeline;" #elif MC_VER >= V1_21_3 + "Ljava/util/function/Function;" #endif + "Lnet/minecraft/resources/ResourceLocation;IIFFIIIII)V", ordinal = 0), method = "renderLogo(Lnet/minecraft/client/gui/GuiGraphics;IFI)V")
+    private static void renderLogo(GuiGraphics instance, #if MC_VER >= V1_21_6 RenderPipeline #elif MC_VER >= V1_21_3 Function<ResourceLocation, RenderType> #endif pipeline, ResourceLocation atlas, int x, int y, float u, float v, int width, int height, int textureWidth, int textureHeight, int color) {
+        GuiRenderer.blitSprite(instance, "GUI_TEXTURED", ResourceBridge.spriteDefaultNamespace("title/minecraft"), new Rectangle(x, y, width, height), new Rectangle((int) u, (int) v, textureWidth, textureHeight), CUI.cuiConfig.getTextColor(color));
     }
-    #else
-    #if MC_VER >= V1_21
-    @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;blit(Lnet/minecraft/resources/ResourceLocation;IIFFIIII)V"), method = "renderLogo(Lnet/minecraft/client/gui/GuiGraphics;IFI)V")
-    private static void injected1(GuiGraphics instance, ResourceLocation atlasLocation, int x, int y, float uOffset, float vOffset, int width, int height, int textureWidth, int textureHeight) {
 
-        #if MC_VER <= V1_21_1 instance.setColor(CUI.cuiConfig.r, CUI.cuiConfig.g, CUI.cuiConfig.b, 1); #endif
-
-        instance.blit(atlasLocation, x, y, uOffset, vOffset, width, height, textureWidth, textureHeight);
-
-        #if MC_VER <= V1_21_1 instance.setColor(1, 1, 1, 1); #endif
+    @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;blit(" #if MC_VER >= V1_21_6 + "Lcom/mojang/blaze3d/pipeline/RenderPipeline;" #elif MC_VER >= V1_21_3 + "Ljava/util/function/Function;" #endif + "Lnet/minecraft/resources/ResourceLocation;IIFFIIIII)V", ordinal = 1), method = "renderLogo(Lnet/minecraft/client/gui/GuiGraphics;IFI)V")
+    private static void renderEdition(GuiGraphics instance, #if MC_VER >= V1_21_6 RenderPipeline #elif MC_VER >= V1_21_3 Function<ResourceLocation, RenderType> #endif pipeline, ResourceLocation atlas, int x, int y, float u, float v, int width, int height, int textureWidth, int textureHeight, int color) {
+        GuiRenderer.blitSprite(instance, "GUI_TEXTURED", ResourceBridge.spriteDefaultNamespace("title/edition"), new Rectangle(x, y, width, height), new Rectangle((int) u, (int) v, textureWidth, textureHeight), CUI.cuiConfig.getTextColor(color));
     }
     #else
     @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;setColor(FFFF)V"), method = "renderLogo(Lnet/minecraft/client/gui/GuiGraphics;IFI)V")
-    private void injected1(GuiGraphics instance, float red, float green, float blue, float alpha) {
-        instance.setColor(CUI.cuiConfig.r, CUI.cuiConfig.g, CUI.cuiConfig.b, alpha);
+    private void color(GuiGraphics guiGraphics, float red, float green, float blue, float alpha) {
+        GuiRenderer.setShaderColor(guiGraphics, CUI.cuiConfig.getTextColor(-1, alpha));
     }
 
     @Inject(at = @At(value = "TAIL"), method = "renderLogo(Lnet/minecraft/client/gui/GuiGraphics;IFI)V")
-    private void injected1(GuiGraphics guiGraphics, int screenWidth, float transparency, int height, CallbackInfo ci) {
-        guiGraphics.setColor(1, 1, 1, 1);
+    private void clearColor(GuiGraphics guiGraphics, int screenWidth, float transparency, int height, CallbackInfo ci) {
+        GuiRenderer.clearShaderColor(guiGraphics);
     }
-    #endif
-    #endif
     #endif
 }
