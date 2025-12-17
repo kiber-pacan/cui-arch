9084711e1f67125a5b4c893099598a5c87a19c19
kiber-pacan <markpustog@gmail.com>
2025-12-17 21:03:05 +0500

    refactoring and stuff

---
+++ common/src/main/java/com/cui/mixin/client/misc/widgets/scroll/ScrollerMixin.java
@@ -0,0 +1,66 @@
+package com.cui.mixin.client.misc.widgets.scroll;
+
+#if MC_VER >= V1_21_6 import com.mojang.blaze3d.pipeline.RenderPipeline; #endif
+#if MC_VER >= V1_21_4 import net.minecraft.client.gui.components.AbstractScrollArea; #endif
+import org.spongepowered.asm.mixin.Mixin;
+        import net.minecraft.client.Minecraft;
+
+import com.cui.core.CUI;
+#if MC_VER >= V1_21_6 import com.cui.mixin.client.book.RecipeBookMixin;
+import com.mojang.blaze3d.pipeline.RenderPipeline; #endif
+
+
+#if MC_VER >= V1_21_3 import net.minecraft.client.gui.screens.inventory.AbstractRecipeBookScreen; #endif
+import net.minecraft.client.gui.screens.inventory.CraftingScreen;
+#if MC_VER < V1_21_1
+#endif
+import net.minecraft.client.gui.screens.recipebook.RecipeBookComponent;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.inventory.CraftingMenu;
+import net.minecraft.world.item.Items;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiGraphics;
+
+#if MC_VER <= V1_21_6
+
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+#endif
+
+#if MC_VER >= V1_21_6
+import net.minecraft.client.DeltaTracker;
+import net.minecraft.core.component.DataComponents;
+#endif
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Font;
+import net.minecraft.client.gui.Gui;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Rarity;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+
+// Scroller element in screens
+
+@Mixin(#if MC_VER >= V1_21_6 AbstractScrollArea.class #else Minecraft.class #endif)
+public class ScrollerMixin {
+    #if MC_VER >= V1_21_6
+    @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;blitSprite(Lcom/mojang/blaze3d/pipeline/RenderPipeline;Lnet/minecraft/resources/ResourceLocation;IIII)V"), method = "renderScrollbar")
+    private static void injected(GuiGraphics instance, RenderPipeline pipeline, ResourceLocation sprite, int x, int y, int width, int height) {
+        instance.blitSprite(pipeline, sprite, x, y, width, height, CUI.cuiConfig.getRGB());
+    }
+    #endif
+}
--- common/src/main/java/com/cui/mixin/client/misc/widgets/scroll/SelectionListMixin.java
+++ common/src/main/java/com/cui/mixin/client/misc/widgets/scroll/SelectionListMixin.java
@@ -1,6 +1,7 @@
-package com.cui.mixin.client.screen.misc;
+package com.cui.mixin.client.misc.widgets.scroll;
 
 
+import com.cui.abs.core.data.Rectangle;
 import com.cui.abs.core.rendering.gui.GuiRenderer;
 import com.cui.core.CUI;
 import com.mojang.authlib.minecraft.client.MinecraftClient;
@@ -15,6 +16,7 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.components.ContainerObjectSelectionList;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -39,6 +41,10 @@ public abstract class SelectionListMixin #if MC_VER < V1_21 && MC_VER == V1_20_4
     @Shadow protected int y1;
     @Shadow protected int x1;
     @Shadow protected int x0;
+
+    @Shadow protected int width;
+    @Shadow protected int height;
+    @Shadow private boolean renderTopAndBottom;
     #endif
 
     @Shadow public double getScrollAmount() {
@@ -46,17 +52,60 @@ public abstract class SelectionListMixin #if MC_VER < V1_21 && MC_VER == V1_20_4
     }
 
     @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/components/AbstractSelectionList;enableScissor(Lnet/minecraft/client/gui/GuiGraphics;)V", shift = At.Shift.AFTER), method = #if MC_VER == V1_20_4 "renderWidget" #else "render" #endif)
-    private void render3(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick, CallbackInfo ci) {
+    private void fillGradient1(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick, CallbackInfo ci) {
         guiGraphics.setColor(1.0F, 1.0F, 1.0F, 1.0F);
         float[] hsv = CUI.cuiConfig.getHSV();
         guiGraphics.fillGradient(
-                #if MC_VER == V1_20_4 this.getX(), this.getY(), this.getRight(), this.getBottom(), #else
+                #if MC_VER == V1_20_4
+                this.getX(), this.getY(), this.getRight(), this.getBottom(),
+                #else
                 this.x0, this.y0, (int) (this.x1), (int) ((this.y1 + (int)this.getScrollAmount())),
                 #endif
                 ((int)(CUI.cuiConfig.a * 255) << 24) | (Color.getHSBColor(hsv[0], hsv[1], hsv[2] / 2.7f).getRGB() & 0x00FFFFFF),
                 ((int)(CUI.cuiConfig.a * 255) << 24) | (Color.getHSBColor(hsv[0], hsv[1], hsv[2] / 5.0f).getRGB() & 0x00FFFFFF)
         );
     }
+
+    #if MC_VER <= V1_20_1
+    @Inject(at = @At(value = "TAIL"), method = #if MC_VER == V1_20_4 "renderWidget" #else "render" #endif)
+    private void fillGradient2(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick, CallbackInfo ci) {
+        if (this.renderTopAndBottom) {
+        float[] hsv = Color.RGBtoHSB((int) (CUI.cuiConfig.r * 255.0f), (int) (CUI.cuiConfig.g * 255.0f), (int) (CUI.cuiConfig.b * 255.0f), null);
+        guiGraphics.fillGradient(
+                #if MC_VER == V1_20_4
+                this.getX(), 0, this.getRight(), this.getY(),
+                #else
+                this.x0, 0, this.x1, this.y0,
+                #endif
+
+                ((int) (CUI.cuiConfig.a * 255) << 24) | (Color.getHSBColor(hsv[0], hsv[1], hsv[2] / 2.7f).getRGB() & 0x00FFFFFF),
+                ((int) (CUI.cuiConfig.a * 255) << 24) | (Color.getHSBColor(hsv[0], hsv[1], hsv[2] / 5.0f).getRGB() & 0x00FFFFFF)
+        );
+
+        guiGraphics.fillGradient(
+                #if MC_VER == V1_20_4
+                this.getX(), this.getBottom(), this.getRight(), this.getHeight(),
+                #else
+                this.x0, this.y1, this.x1, this.height,
+                #endif
+                ((int) (CUI.cuiConfig.a * 255) << 24) | (Color.getHSBColor(hsv[0], hsv[1], hsv[2] / 2.7f).getRGB() & 0x00FFFFFF),
+                ((int) (CUI.cuiConfig.a * 255) << 24) | (Color.getHSBColor(hsv[0], hsv[1], hsv[2] / 5.0f).getRGB() & 0x00FFFFFF)
+        );
+        }
+    }
+    #endif
+
+    #if MC_VER >= V1_20_4
+    @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;blitSprite(Lnet/minecraft/resources/ResourceLocation;IIII)V"), method = "renderWidget")
+    private static void fill1(GuiGraphics instance, ResourceLocation sprite, int x, int y, int width, int height) {
+        GuiRenderer.blitSprite(instance, "GUI_TEXTURED", sprite, new Rectangle(x, y, width, height), CUI.cuiConfig.getRGB());
+    }
+    #else
+    @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;fill(IIIII)V"), method = "render")
+    private static void fill1(GuiGraphics instance, int minX, int minY, int maxX, int maxY, int color) {
+        instance.fill(minX, minY, maxX, maxY, CUI.cuiConfig.getTextColor(color));
+    }
+    #endif
 
     #if MC_VER >= V1_20_4
     @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;setColor(FFFF)V", ordinal = 0), method = "renderWidget")
